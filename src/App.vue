35b8f1ee799985b98e9a23b19f4a378bfcd52388
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-02-04 12:38:35 +0100

    First commit.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+		<HeaderComponent></HeaderComponent>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+	import { Component, Prop, Vue } from 'vue-property-decorator';
+	import HeaderComponent from './views/shared-views/Header.vue';
+
+	@Component({
+		components: {
+			HeaderComponent
+		}
+	})
+	export default class AppComponent extends Vue {
+
+	}
+</script>
+
 
 <style lang="scss">
 #app {
@@ -17,7 +31,6 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/components/shared-views/Header.vue
@@ -0,0 +1,36 @@
+<template>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<router-link to="/portfolio" class="navbar-brand">Stock Trader</router-link>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<router-link class="nav-item active" activeClass="active" to="/portfolio" tag="li"><a class="nav-link">Portfolios</a></router-link>
+				<router-link class="nav-item active" activeClass="active" to="/portfolio" tag="li"><a class="nav-link">Stocks</a></router-link>
+			</ul>
+			<div class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</div>
+		</div>
+	</nav>
+
+</template>
+
+<script lang="ts">
+	import { Component, Prop, Vue } from 'vue-property-decorator';
+
+	@Component
+	export default class HeaderComponent extends Vue {
+		mounted() {
+			console.log('Header Component');
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+
+</style>
